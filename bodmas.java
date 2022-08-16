5886202094f820b17eec449c3c390e6721fae3a8
mukeshsuvendheran <109419653+mukeshsuvendheran@users.noreply.github.com>
2022-08-17 00:43:42 +0530

    Create bodmas.java

---
+++ bodmas.java
@@ -0,0 +1,10 @@
+public class bodmas {
+  public static void main(String[] args) {
+    int x = 25;
+    int y = 4;
+    int b = 1000;
+    int a = 45;
+    int z = x*y+b/a;
+    System.out.println(z);
+  }
+}
