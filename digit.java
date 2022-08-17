df01a2887ec62a30c3c78d4388664662ee92bb0c
mukeshsuvendheran <109419653+mukeshsuvendheran@users.noreply.github.com>
2022-08-18 00:27:18 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,8 @@
+public class digit {
+  public static void main(String[] args) {
+    long M = 437827642878L;
+    long N = 134948762478L;
+    long P = 454948762478L;
+    System.out.println(M * N + P);  
+  }
+}
